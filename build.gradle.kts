392082265cfbc5cd66d9d6f067aa105c8d6c3d6e
RuanCampello <ruancampello999@gmail.com>
2025-10-07 16:07:54 -0300

    feat: initial setup

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    kotlin("jvm") version "2.2.10"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(24)
+}
+
+application {
+  mainClass.set("MainKt")
+}
